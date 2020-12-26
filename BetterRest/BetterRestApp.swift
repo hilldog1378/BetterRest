23bbbbe2248a65b9f20fe8f609ccb794c95e8be8
austin hill <hilldog1378@gmail.com>
2020-12-25 19:07:46 -0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Austin Hill on 12/25/20.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
